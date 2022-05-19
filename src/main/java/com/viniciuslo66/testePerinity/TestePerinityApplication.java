da61d31cd09f3cb4ff37c20c83c47f6caa87f999
viniciuslo66 <viniciuslo66@gmail.com>
2022-05-18 21:54:47 -0300

    first commit

---
+++ src/main/java/com/viniciuslo66/testePerinity/TestePerinityApplication.java
@@ -0,0 +1,13 @@
+package com.viniciuslo66.testePerinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePerinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestePerinityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciuslo66/testePerinity/TestePerinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciuslo66.testePerinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePerinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
